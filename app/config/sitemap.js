b77412565b1dac9e36f9d19ed58c26944e1c90c2
Vidar Eldøy <vidar@eldoy.com>
2021-11-12 16:18:43 +0100

    Update sitemap

--- app/config/sitemap.js
+++ app/config/sitemap.js
@@ -2,13 +2,13 @@ const path = require('path')
 const { tree } = require('extras')
 const root = path.join(process.cwd(), 'app', 'pages')
 const files = tree('app/pages').map(f => {
-  return f.replace(root, '').replace(/\.(md|js)/, '.html')
+  return f.replace(root, '').replace(/\.(md|js)/, '')
 })
 
 const lastmodfile = 'app/pages/index.js'
 const urls = [
   { url: '/', lastmodfile, changefreq: 'daily', priority: 1.0 },
-  { url: '/about.html', lastmodfile, changefreq: 'weekly', priority: 0.6 }
+  { url: '/about', lastmodfile, changefreq: 'weekly', priority: 0.6 }
 ]
 
 files.forEach(function(url) {
